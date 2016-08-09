69ef7058f3e1f57be8ef47e62007cbaa78ff3237
Paul Hobson <pmhobson@gmail.com>
2016-08-09 10:02:06 -0700

    setup project dir

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
@@ -3,3 +3,8 @@
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
+
+
+from {{ cookiecutter.project_slug }} import *
+from . import viz
+from .tests import test
